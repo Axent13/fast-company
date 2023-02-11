87a6102e1ff76dc4e54732422b48e929fa46d48f
Axent13 <axent13@yandex.ru>
2023-02-11 08:41:28 +0700

    Задание 3

--- src/app/store/comments.js
+++ src/app/store/comments.js
@@ -1,5 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice } from "@reduxjs/toolkit";
+import { nanoid } from "nanoid";
 import commentService from "../services/comment.service";
+import localStorageService from "../services/localStorage.service";
 
 const commentsSlice = createSlice({
     name: "comments",
@@ -19,12 +21,40 @@ const commentsSlice = createSlice({
         commentsRequestFailed: (state, action) => {
             state.error = action.payload;
             state.isLoading = false;
+        },
+        commentAdded: (state, action) => {
+            if (!Array.isArray(state.entities)) {
+                state.entities = [];
+            }
+            state.entities.push(action.payload);
+        },
+        commentAddFailed: (state, action) => {
+            state.error = action.payload;
+        },
+        commentRemoveFailed: (state, action) => {
+            state.error = action.payload;
+        },
+        commentRemoved: (state, action) => {
+            state.entities = state.entities.filter(
+                (comment) => comment._id !== action.payload
+            );
         }
     }
 });
 
 const { reducer: commentsReducer, actions } = commentsSlice;
-const { commentsRequested, commentsReceived, commentsRequestFailed } = actions;
+const {
+    commentsRequested,
+    commentsReceived,
+    commentsRequestFailed,
+    commentAdded,
+    commentAddFailed,
+    commentRemoved,
+    commentRemoveFailed
+} = actions;
+
+const commentAddRequested = createAction("comments/commentAddRequested");
+const commentRemoveRequested = createAction("comments/commentRemoveRequested");
 
 export const loadCommentsList = (userId) => async (dispatch) => {
     dispatch(commentsRequested());
@@ -33,6 +63,37 @@ export const loadCommentsList = (userId) => async (dispatch) => {
         dispatch(commentsReceived(content));
     } catch (error) {
         dispatch(commentsRequestFailed(error.message));
+    }
+};
+
+export const addComment = (payload) => async (dispatch) => {
+    dispatch(commentAddRequested());
+    console.log("payload:", payload);
+    const comment = {
+        ...payload,
+        _id: nanoid(),
+        created_at: Date.now(),
+        userId: localStorageService.getUserId()
+    };
+    try {
+        console.log(comment);
+        const { content } = await commentService.createComment(comment);
+        console.log("content:", content);
+        dispatch(commentAdded(content));
+    } catch (error) {
+        dispatch(commentAddFailed(error.message));
+    }
+};
+
+export const removeComment = (commentId) => async (dispatch) => {
+    dispatch(commentRemoveRequested());
+    try {
+        const { content } = await commentService.removeComment(commentId);
+        if (content === null) {
+            dispatch(commentRemoved(commentId));
+        }
+    } catch (error) {
+        dispatch(commentRemoveFailed(error.message));
     }
 };
 
